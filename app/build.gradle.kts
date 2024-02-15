a8dd3ff3a3837fa74e80b0f24ea20b5dc4e6f3e9
komal1101peoplesuite <komal.shinde@peoplesuitehcm.co.in>
2024-02-15 14:10:31 +0530

    Gradle dependency added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,10 +34,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    //noinspection GradleCompatible,GradleCompatible
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,4 +49,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //mvvm dependencies
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bump tech.glide:glide:4.11.0")
+    implementation ("com.square.retrofit2:retrofit:2.9.0")
+    implementation ("com.square.retrofit2:converter-gson:2.9.0")
+    implementation ("android.arch.lifecycle:extensions:1.1.1")
+    implementation ("androidx.lifecycle:lifecycle-view-model-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation ("com.google.dagger:dagger:2.42")
+    kapt ("com.google.dagger:dagger-compiler:2.42")
+    implementation ("androidx.browser:browser:1.7.0")
+
 }
