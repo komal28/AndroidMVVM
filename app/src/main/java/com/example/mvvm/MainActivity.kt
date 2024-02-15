daa2e7b642cff9a9fe67b12742c7ec4f325b8d7f
komal1101peoplesuite <komal.shinde@peoplesuitehcm.co.in>
2024-02-15 13:32:56 +0530

    Toast Added

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.mvvm
 
+import android.health.connect.datatypes.units.Length
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this,"Welcome to Splash Screen",Toast.LENGTH_LONG).show()
     }
 }
